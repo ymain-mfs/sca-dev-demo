2764f8407d08d2d1146031734189a0ac77fdc218
Yosef Main <ymain@mfssupply.com>
2019-08-28 15:40:24 -0400

    widget module 18-02 moved into generationn directory where it should have been

---
+++ Modules/generationn/Widget@genn_18-02/JavaScript/Widget.Edit.View.js
@@ -0,0 +1,64 @@
+define(
+    'Widget.Edit.View'
+,   [   'widget_edit.tpl'
+    ,   'Backbone'
+    ,   'Backbone.FormView'
+    ]
+,   function (
+        widget_edit_tpl
+    ,   Backbone
+    ,   BackboneFormView    
+    )
+{
+    'use strict';
+
+    return Backbone.View.extend({
+
+        template: widget_edit_tpl
+
+    ,   events: {
+            'submit form': 'saveForm'
+        }
+
+    ,   initialize: function (options)
+        {
+            this.title = this.model.isNew() ? 'Add Widget' : 'Edit Widget';
+
+            BackboneFormView.add(this);
+        }
+
+    ,   bindings: {
+            '[name="color"]': 'color'
+        ,   '[name="size"]': 'size'
+        //, '[name="shape"]': 'shape'
+        }       
+
+    ,   render: function ()
+        {
+            Backbone.View.prototype.render.apply(this, arguments);
+        }
+
+    ,   getContext: function ()
+        {
+            var widgetShapes = SC.ENVIRONMENT.WIDGET_SHAPES;
+
+            var model = this.model;
+            
+            for (var i=0; i < widgetShapes.length; i++){
+                if (widgetShapes[i].name == model.get('shape')) {
+                    widgetShapes[i].isSelected = true;
+                } else {
+                    widgetShapes[i].isSelected = false;
+                }
+            }
+
+            return {
+                isWidgetNew: this.model.isNew()
+            ,   color: this.model.get('color')
+            ,   size: this.model.get('size')
+            ,   shape: this.model.get('shape')
+            ,   widgetShapes: widgetShapes
+            };
+        }
+    });
+});
---
+++ Modules/generationn/Widget@genn_18-02/XHR Requests/xhr_get_all_request.js
@@ -0,0 +1,6 @@
+var xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'services/Widget.Service.ss');
+
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.send();
